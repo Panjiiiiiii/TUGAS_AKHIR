9f40fa2058ecb7e94092678733a806f07bf44e3f
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2024-05-02 13:00:06 +0700

    integrasi dashboard admin

--- Backend/routes/pemesanan.route.js
+++ Backend/routes/pemesanan.route.js
@@ -9,5 +9,7 @@ app.use(express.json());
 
 app.get("/getUser", authorize, isCustomer, validateUser, pemesanan.getAllpemesanan);
 app.get("/getAdmin", authorize, isAdmin, pemesanan.getAllpemesanan);
+app.get("/checkin", authorize, isAdmin, pemesanan.getCheckIn)
+app.get("/checkout", authorize, isAdmin, pemesanan.getCheckOut)
 
 module.exports = app;
